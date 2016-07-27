b28c80936338f0cd2a2289b557b008a86ccfc54e
CHENBIEBIE <921212200@qq.com>
2016-07-27 10:56:38 +0800

    One To One 第二种形式

---
+++ nate1/src/test/java/com/kaishengit/test/OneToOne2TestCase.java
@@ -0,0 +1,23 @@
+package com.kaishengit.test;
+
+
+import com.kaishengit.pojo.Topic;
+import com.kaishengit.util.HibernateUtil;
+import org.hibernate.Session;
+import org.junit.Test;
+
+public class OneToOne2TestCase {
+
+    @Test
+    public void testFind() {
+
+        Session session = HibernateUtil.getSession();
+        session.beginTransaction();
+
+        Topic topic = (Topic) session.get(Topic.class,1);
+        System.out.println(topic.getTitle());
+        System.out.println(topic.getTopicContent().getContent());
+
+        session.getTransaction().commit();
+    }
+}
